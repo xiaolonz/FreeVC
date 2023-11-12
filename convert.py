427a8657cad32a34eff530b9be9542e07cffb4f7
xiaolonz <xiaolonz@cisco.com>
2023-11-12 22:25:23 +0800

    support convert by bat

--- convert.py
+++ convert.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import argparse
 import torch
@@ -19,8 +20,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--hpfile", type=str, default="configs/freevc.json", help="path to json config file")
     parser.add_argument("--ptfile", type=str, default="checkpoints/freevc.pth", help="path to pth file")
-    parser.add_argument("--txtpath", type=str, default="convert.txt", help="path to txt file")
-    parser.add_argument("--outdir", type=str, default="output/freevc", help="path to output dir")
+    # parser.add_argument("--txtpath", type=str, default="convert.txt", help="path to txt file")
+    parser.add_argument("--targetfile", type=str, default="/data4/xiaolonz/Vctk/vctk-16k/p243/p243_394.wav", help="target "
+                                                                                                              "wav")
+    parser.add_argument("--srcpath", type=str, default="/data4/xiaolonz/Vctk/vctk_16k_all", help="src dir")
+    parser.add_argument("--outdir", type=str, default="/data4/xiaolonz/Vctk/vctk_16k_out", help="out dir")
     parser.add_argument("--use_timestamp", default=False, action="store_true")
     args = parser.parse_args()
     
@@ -43,40 +47,43 @@ if __name__ == "__main__":
         print("Loading speaker encoder...")
         smodel = SpeakerEncoder('speaker_encoder/ckpt/pretrained_bak_5805000.pt')
 
-    print("Processing text...")
-    titles, srcs, tgts = [], [], []
-    with open(args.txtpath, "r") as f:
-        for rawline in f.readlines():
-            title, src, tgt = rawline.strip().split("|")
-            titles.append(title)
-            srcs.append(src)
-            tgts.append(tgt)
+    # print("Processing text...")
+    # titles, srcs, tgts = [], [], []
+    # with open(args.txtpath, "r") as f:
+    #     for rawline in f.readlines():
+    #         title, src, tgt = rawline.strip().split("|")
+    #         titles.append(title)
+    #         srcs.append(src)
+    #         tgts.append(tgt)
+
+    # tgt
+    tgt = args.targetfile
+    wav_tgt, _ = librosa.load(tgt, sr=hps.data.sampling_rate)
+    wav_tgt, _ = librosa.effects.trim(wav_tgt, top_db=20)
+    if hps.model.use_spk:
+        g_tgt = smodel.embed_utterance(wav_tgt)
+        g_tgt = torch.from_numpy(g_tgt).unsqueeze(0).cuda()
+    else:
+        wav_tgt = torch.from_numpy(wav_tgt).unsqueeze(0).cuda()
+        mel_tgt = mel_spectrogram_torch(
+            wav_tgt,
+            hps.data.filter_length,
+            hps.data.n_mel_channels,
+            hps.data.sampling_rate,
+            hps.data.hop_length,
+            hps.data.win_length,
+            hps.data.mel_fmin,
+            hps.data.mel_fmax
+        )
 
     print("Synthesizing...")
     with torch.no_grad():
-        for line in tqdm(zip(titles, srcs, tgts)):
-            title, src, tgt = line
-            # tgt
-            wav_tgt, _ = librosa.load(tgt, sr=hps.data.sampling_rate)
-            wav_tgt, _ = librosa.effects.trim(wav_tgt, top_db=20)
-            if hps.model.use_spk:
-                g_tgt = smodel.embed_utterance(wav_tgt)
-                g_tgt = torch.from_numpy(g_tgt).unsqueeze(0).cuda()
-            else:
-                wav_tgt = torch.from_numpy(wav_tgt).unsqueeze(0).cuda()
-                mel_tgt = mel_spectrogram_torch(
-                    wav_tgt, 
-                    hps.data.filter_length,
-                    hps.data.n_mel_channels,
-                    hps.data.sampling_rate,
-                    hps.data.hop_length,
-                    hps.data.win_length,
-                    hps.data.mel_fmin,
-                    hps.data.mel_fmax
-                )
+        for srcfile in glob.glob(os.path.join(args.srcpath, '*.wav')):
             # src
-            wav_src, _ = librosa.load(src, sr=hps.data.sampling_rate)
-            wav_src = torch.from_numpy(wav_src).unsqueeze(0).cuda()
+            basename = os.path.basename(srcfile)
+            src, extension = os.path.splitext(basename)
+            wav_src1, _ = librosa.load(srcfile, sr=hps.data.sampling_rate)
+            wav_src = torch.from_numpy(wav_src1).unsqueeze(0).cuda()
             c = utils.get_content(cmodel, wav_src)
             
             if hps.model.use_spk:
@@ -86,7 +93,14 @@ if __name__ == "__main__":
             audio = audio[0][0].data.cpu().float().numpy()
             if args.use_timestamp:
                 timestamp = time.strftime("%m-%d_%H-%M", time.localtime())
-                write(os.path.join(args.outdir, "{}.wav".format(timestamp+"_"+title)), hps.data.sampling_rate, audio)
+                write(os.path.join(args.outdir, "{}.wav".format(timestamp+"_"+srcfile)), hps.data.sampling_rate, audio)
             else:
-                write(os.path.join(args.outdir, f"{title}.wav"), hps.data.sampling_rate, audio)
+                delay = 160*5
+                write(os.path.join(args.outdir, f"{src}_converted.wav"), hps.data.sampling_rate,
+                      audio[3200+delay:-3200])
+                len_audio = len(audio[3200+delay:-3200])
+                write(os.path.join(args.outdir, f"{src}_origin.wav"), hps.data.sampling_rate,
+                      wav_src1[3200:3200+len_audio])
+
+
             
